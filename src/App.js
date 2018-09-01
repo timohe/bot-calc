89f98f7c9c1334ddebe7d412de3e3554b654fd55
Timo Hegnauer <t.hegnauer@gmail.com>
2018-09-01 21:06:13 +0200

    add all inputs, send data to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Form from './components/Form'
+import Button from '@material-ui/core/Button';
 import './App.css';
 
 class App extends Component {
@@ -10,7 +11,6 @@ class App extends Component {
       name: 'blabla',
       interestRatePerDay: 1.00,
       investmentLength: 99,
-      gain: 0,
       doTakeOutCapital: false,
       initialCapital: 1000,
       investedCapital: [],
@@ -18,25 +18,28 @@ class App extends Component {
     }
   }
 
-  calculate = (interestRatePerDay, investmentLength, gain, doTakeOutCapital, initialCapital) => {
+  calculate = () => {
+    var gain;
     var gainArray = [];
     var capitalArray = [];
-    var currentlyInvested = initialCapital;
+    var currentlyInvested = this.state.initialCapital;
 
     for (var periods = 1; periods <= 6; periods++) {
+      console.log(currentlyInvested);
       capitalArray.push(currentlyInvested)
-      for (var day = 0; day <= investmentLength; day++) {
-        currentlyInvested += currentlyInvested * (interestRatePerDay / 100);
-        gain += currentlyInvested * (interestRatePerDay / 100);
+      for (var day = 0; day <= this.state.investmentLength; day++) {
+        currentlyInvested += currentlyInvested * (this.state.interestRatePerDay / 100);
+        gain += currentlyInvested * (this.state.interestRatePerDay / 100);
       }
+      // console.log(gain);
+      
       gainArray.push(gain);
       //after the x days are gone, you got your capital back in rates so this is now included in the gains
       currentlyInvested = gain;
-      if (doTakeOutCapital) {
+      if (this.state.doTakeOutCapital) {
         currentlyInvested -= capitalArray[capitalArray.length - 1];
       }
     }
-
     this.setState({
       gains: gainArray,
       investedCapital: capitalArray
@@ -50,20 +53,47 @@ class App extends Component {
     });
   };
 
+  handleCheckboxChange = (fieldName) => event => {
+    this.setState({
+      [fieldName]: event.target.checked,
+    });
+  };
+
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        {/* <p className="App-intro">
           FromChild: {this.state.name}
         </p>
+        <p className="App-intro">
+          Checkbox: {this.state.doTakeOutCapital.toString()}
+        </p> */}
+        <p className="App-intro">
+          Gain: {this.state.gains.toString()}
+        </p>
+        <p className="App-intro">
+          Capital: {this.state.investedCapital.toString()}
+        </p>
+
         <Form
           handleChange={this.handleChange}
+          handleCheckboxChange={this.handleCheckboxChange}
+
         />
+        <Button 
+          variant="contained" 
+          color="secondary" 
+          onClick={() => this.calculate(this.state.interestRatePerDay, this.state.investmentLength, this.state.doTakeOutCapital, this.state.initialCapital)}
+          >
+          Secondary
+        </Button>
       </div>
+
     );
   }
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,24 +1,55 @@
 import React, { Component } from 'react';
 import TextField from '@material-ui/core/TextField';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 class Form extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'initial'
     };
   }
 
   render() {
     return (
       <div>
-        <TextField
-          id="name"
-          label="Name"
-          placeholder="Timo"
-          onChange={this.props.handleChange('name')}
-          margin="normal"
-        />
+        <div>
+          <TextField
+            label="Daily interest"
+            placeholder="1.5"
+            onChange={this.props.handleChange('name')}
+            margin="normal"
+            type="number"
+          />
+        </div>
+        <div>
+          <TextField
+            label="Duration in days"
+            placeholder="90"
+            onChange={this.props.handleChange('investmentLength')}
+            margin="normal"
+            type="number"
+          />
+        </div>
+        <div>
+          <TextField
+            label="Initial investment"
+            placeholder="1000"
+            onChange={this.props.handleChange('initialCapital')}
+            margin="normal"
+            type="number"
+          />
+        </div>
+        <div>
+          <FormControlLabel
+            control={
+              <Checkbox
+                onChange={this.props.handleCheckboxChange('doTakeOutCapital')}
+              />
+            }
+            label="Take out initial investment"
+          />
+        </div>
       </div>
     )
   }
